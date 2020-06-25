639f38dceedeef60d343996b85e4227526d4ce82
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-06-25 15:00:08 -0500

    Add task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
+""" Base Model Module """
 import uuid
 from datetime import datetime
 
 
 class BaseModel():
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
+        if kwargs and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
-        
+
     def to_dict(self):
         dic = self.__dict__
         dic['created_at'] = datetime.isoformat(dic['created_at'])
@@ -25,4 +34,4 @@ class BaseModel():
 
 
 a = BaseModel()
-print(a.__str__())
+
