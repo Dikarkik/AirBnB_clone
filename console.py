e7d00e94dde6a1c9efaa99040ce4e184e20b6d98
Diana Carolina Quintero <karodev3@gmail.com>
2020-06-26 21:16:18 +0000

    Add method do_update

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ class HBNBCommand(cmd.Cmd):
     """ Command interpreter """
     prompt = "(hbnb) "
     file = None
+    classes = ['BaseModel']
 
     def emptyline(self):
         return
@@ -78,16 +79,48 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, line):
         """ Prints all string representation of all \
         instances based or not on the class name """
-        classes = ['BaseModel']
         if line == "":
             l = [str(value) for key, value in storage.all().items()]
         else:
             tokens = line.split(" ")
-            if tokens[0] not in classes:
+            if tokens[0] not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
             l = [str(value) for key, value in storage.all().items() if type(value).__name__ == tokens[0]]
         print(l)
+
+    def do_update(self, line):
+        """ Updates an instance based on the class name and id
+        by adding or updating attribute (save the change into the JSON file).
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+        """
+        if line == "":
+            print("** class name missing **")
+            return
+        tokens = line.split(" ")
+        if tokens[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(tokens) < 2:
+            print("** instance id missing **")
+            return
+        id_obj = str(tokens[0] + "." + tokens[1])
+        if id_obj not in storage.all():
+            print("** no instance found **")
+            return
+        if len(tokens) < 3:
+            print("** attribute name missing **")
+            return
+        if len(tokens) < 4:
+            print("** value missing **")
+            return
+        if tokens[3][0] == '"':
+            value = " ".join(token for token in tokens[3:])
+            value = value.split('"')[1]
+        else:
+            value = tokens[3]
+        setattr(storage.all()[tokens[0] + "." + tokens[1]], tokens[2], value)
+        storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
